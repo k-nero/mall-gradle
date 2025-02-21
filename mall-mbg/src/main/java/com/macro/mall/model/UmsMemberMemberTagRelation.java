4d969b002d173cdb43761a554ca732501101fcf2
Nero <67089844+k-nero@users.noreply.github.com>
2025-02-22 01:16:52 +0700

    refactor: migrate from javax.servlet to jakarta.servlet and clean up code

--- mall-mbg/src/main/java/com/macro/mall/model/UmsMemberMemberTagRelation.java
+++ mall-mbg/src/main/java/com/macro/mall/model/UmsMemberMemberTagRelation.java
@@ -1,8 +1,13 @@
 package com.macro.mall.model;
 
-import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
 import java.io.Serializable;
 
+@Setter
+@Getter
 public class UmsMemberMemberTagRelation implements Serializable {
     private Long id;
 
@@ -10,43 +15,18 @@ public class UmsMemberMemberTagRelation implements Serializable {
 
     private Long tagId;
 
+    @Serial
     private static final long serialVersionUID = 1L;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public Long getTagId() {
-        return tagId;
-    }
-
-    public void setTagId(Long tagId) {
-        this.tagId = tagId;
-    }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", memberId=").append(memberId);
-        sb.append(", tagId=").append(tagId);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+        return getClass().getSimpleName() +
+                " [" +
+                "Hash = " + hashCode() +
+                ", id=" + id +
+                ", memberId=" + memberId +
+                ", tagId=" + tagId +
+                ", serialVersionUID=" + serialVersionUID +
+                "]";
     }
 }
